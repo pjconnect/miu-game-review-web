b6575d07f99da58d4312e5ee007be32d992d3a58
pasinduj <pasindu.connect@gmail.com>
2023-06-22 09:44:48 -0500

    UI design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { AllGamesComponent } from './all-games/all-games.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeBannerComponent,
+    AllGamesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: '',
+      component: HomeComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
